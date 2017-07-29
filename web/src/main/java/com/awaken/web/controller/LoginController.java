d86540597511eabc12394058b9ddcfced6e21f4c
FinnBoy <finn0918@163.com>
2017-07-30 06:52:35 +0800

    add https support #2.

--- web/src/main/java/com/awaken/web/controller/LoginController.java
+++ web/src/main/java/com/awaken/web/controller/LoginController.java
@@ -6,8 +6,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login/index")
-    public String login() {
+    @RequestMapping("/login/signin")
+    public String signIn() {
+
+        return "";
+    }
+
+    @RequestMapping("/login/signout")
+    public String signOut() {
+
+        return "";
+    }
+
+    @RequestMapping("/login/signup")
+    public String signUp() {
+
         return "";
     }
 }
