fc7d6e0855487954d07bfb738fb8359c73b9020c
FinnBoy <finn0918@163.com>
2017-08-24 01:04:09 +0800

    update Entity:Permission.

--- resource/src/main/java/com/awaken/resource/entity/permission/Permission.java
+++ resource/src/main/java/com/awaken/resource/entity/permission/Permission.java
@@ -1,6 +1,10 @@
 package com.awaken.resource.entity.permission;
 
 import com.awaken.resource.constant.PermissionDivision;
+import com.awaken.resource.entity.resource.BackendService;
+import com.awaken.resource.entity.resource.Document;
+import com.awaken.resource.entity.resource.FrontendComponent;
+import com.awaken.resource.entity.resource.TablePKValue;
 
 import javax.persistence.Entity;
 
@@ -8,4 +12,22 @@ import javax.persistence.Entity;
 public class Permission {
 
     private PermissionDivision permissionDivision;
+
+    /**
+     * 单个文件（不能设为目录）
+     */
+    private Document document;
+
+    /**
+     * 单条数据
+     * <p>
+     * 与该表主键有关的外键关联表：多（外键表）对一（主键表）、多对多，<br/>
+     * 外键值为该条数据的关联数据可以访问，外键值非该条数据的关联数据无法访问）
+     * </p>
+     */
+    private TablePKValue tablePKValue;
+
+    private BackendService backendService;
+
+    private FrontendComponent frontendComponent;
 }
